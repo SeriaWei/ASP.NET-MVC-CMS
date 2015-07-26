941fe4743a19a19c31943d28fe3c978afc38f54c
Wayne <seriawei@outlook.com>
2015-07-26 22:07:28 +0800

    update dependency

--- Easy.CMS.Web/Global.asax.cs
+++ Easy.CMS.Web/Global.asax.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web.Compilation;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-using Easy.Web.CMS;
-using Microsoft.Practices.Unity;
+﻿using Easy.Web.CMS;
 
 namespace Easy.CMS.Web
 {
