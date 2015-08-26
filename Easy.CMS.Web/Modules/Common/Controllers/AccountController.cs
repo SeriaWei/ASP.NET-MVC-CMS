c869204c04ee1897b9fb4d3dd722324c8c2574f7
Wayne <seriawei@outlook.com>
2015-08-26 22:19:30 +0800

    。

--- Easy.CMS.Web/Modules/Common/Controllers/AccountController.cs
+++ Easy.CMS.Web/Modules/Common/Controllers/AccountController.cs
@@ -5,13 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using Easy.Extend;
+using Easy.Modules.User.Models;
 using Easy.Modules.User.Service;
 
 namespace Easy.CMS.Common.Controllers
 {
     public class AccountController : Controller
     {
-         private readonly IUserService _userService;
+        private readonly IUserService _userService;
 
         public AccountController(IUserService userService)
         {
@@ -28,13 +29,17 @@ namespace Easy.CMS.Common.Controllers
         [HttpPost]
         public ActionResult Login(string userName, string password, string ReturnUrl)
         {
-            // _userService.Login(userName, password);
-            FormsAuthentication.SetAuthCookie(userName, false);
-            if (ReturnUrl.IsNullOrEmpty())
+            var user = _userService.Login(userName, password);
+            if (user != null)
             {
-                return RedirectToAction("Index", "Layout", new { module = "common" });
+                FormsAuthentication.SetAuthCookie(userName, false);
+                if (ReturnUrl.IsNullOrEmpty())
+                {
+                    return RedirectToAction("Index", "Layout", new { module = "common" });
+                }
+                return Redirect(ReturnUrl);
             }
-            return Redirect(ReturnUrl);
+            return View();
         }
     }
 }
