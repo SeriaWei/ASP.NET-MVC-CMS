d5974a7948067536641870b2050bd1ddf51baf00
Wayne <seriawei@gmail.com>
2015-08-28 15:03:46 +0800

    update sth.

---
+++ EasyFrameWork.CMS/HtmlHelperExtend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Easy.Extend;
+
+namespace Easy.Web.CMS
+{
+    public static class HtmlHelperExtend
+    {
+        public static MvcHtmlString SmartLink(this HtmlHelper html, string link, string text)
+        {
+            if (link.IsNullOrEmpty())
+            {
+                link = "/";
+            }
+            return MvcHtmlString.Create("<a target=\"" + ((link.StartsWith("http://") || link.StartsWith("https://")) ? "_blank" : "_self") + "\" href=\"" + link + "\">" + text + "</a>");
+        }
+    }
+}
