90182297c90dc34bc4347254b1036415e8ebbf81
Wayne <seriawei@gmail.com>
2015-08-31 16:22:47 +0800

    style update

--- Build.cmd
+++ Build.cmd
@@ -3,6 +3,7 @@
 @IF NOT EXIST %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe (GOTO :noBuildengine) ELSE GOTO :execute
 :execute
 set MSBuildEmitSolution=1
+set BuildConfiguration=Release
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Easy.CMS.Web.sln 
 @pause
 :noBuildengine
