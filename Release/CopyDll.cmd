a7466723c7a9b7d6f6aec044ed8b16205d33e649
Wayne <seriawei@gmail.com>
2015-09-09 11:53:05 +0800

    release package

--- Release/CopyDll.cmd
+++ Release/CopyDll.cmd
@@ -1,8 +1,11 @@
 @echo off
 for /f %%a in ('dir Temp\Application\Modules /b') do (
 	xcopy Temp\Application\Modules\%%a\bin\Easy.CMS.%%a.dll Temp\Application\bin /S /F /Y
+	@echo Delete Temp\Application\Modules\%%a\bin
 	rd /S /Q Temp\Application\Modules\%%a\bin
+	@echo Delete Temp\Application\Modules\%%a\*.config
 	del Temp\Application\Modules\%%a\*.config
+	@echo Delete Temp\Application\Modules\%%a\*.xml
 	del Temp\Application\Modules\%%a\*.xml
 )
 @pause
