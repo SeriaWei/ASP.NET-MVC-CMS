06fa3041dd58ad094e039f5ad0e73fd626f60f73
Wayne <seriawei@outlook.com>
2016-01-20 21:20:30 +0800

    Statistics

---
+++ Easy.CMS.Web/Modules/Statistics/Controllers/OpenController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Easy.CMS.Statistics.Service;
+using Easy.Data;
+
+namespace Easy.CMS.Statistics.Controllers
+{
+    public class OpenController : Controller
+    {
+
+        public JsonResult Index()
+        {
+            var service = new StatisticsService();
+            if (Request.UrlReferrer != null && service.Count(new DataFilter().Where("Host", OperatorType.Equal, Request.UrlReferrer.Host)) == 0)
+            {
+                service.Add(new Models.Statistics
+                {
+                    Host = Request.UrlReferrer.Host,
+                    IpAddress = Request.UserHostAddress
+                });
+            }
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
