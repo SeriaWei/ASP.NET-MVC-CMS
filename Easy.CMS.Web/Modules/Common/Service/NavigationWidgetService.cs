8000b108778f892c2f8d881bb024405dc21d128a
Wayne <seriawei@gmail.com>
2016-02-28 16:52:03 +0800

    Navigation

--- Easy.CMS.Web/Modules/Common/Service/NavigationWidgetService.cs
+++ Easy.CMS.Web/Modules/Common/Service/NavigationWidgetService.cs
@@ -2,10 +2,10 @@
 using Easy.CMS.Common.ViewModels;
 using Easy.Data;
 using Easy.Web.CMS.Widget;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Easy.Constant;
+using Easy.Extend;
 
 namespace Easy.CMS.Common.Service
 {
@@ -13,7 +13,24 @@ namespace Easy.CMS.Common.Service
     {
         public override WidgetPart Display(WidgetBase widget, HttpContextBase httpContext)
         {
-            var navs = new NavigationService().Get(new Data.DataFilter().OrderBy("DisplayOrder", OrderType.Ascending));
+            var navs = new NavigationService().Get(new DataFilter().OrderBy("DisplayOrder", OrderType.Ascending)).Where(m => m.Status == (int)RecordStatus.Active);
+            string path = "~" + httpContext.Request.Path.ToLower();
+            NavigationEntity current = null;
+            int length = 0;
+            foreach (var navigationEntity in navs)
+            {
+                if (navigationEntity.Url.IsNotNullAndWhiteSpace()
+                    && path.StartsWith(navigationEntity.Url.ToLower())
+                    && length < navigationEntity.Url.Length)
+                {
+                    current = navigationEntity;
+                    length = navigationEntity.Url.Length;
+                }
+            }
+            if (current != null)
+            {
+                current.IsCurrent = true;
+            }
             return widget.ToWidgetPart(new NavigationWidgetViewModel(navs, widget as NavigationWidget));
         }
     }
