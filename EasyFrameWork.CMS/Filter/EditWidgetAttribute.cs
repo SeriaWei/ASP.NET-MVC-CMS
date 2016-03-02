738062a3118f6974e11226ae1ad79155aa0a58c1
Wayne <seriawei@gmail.com>
2016-03-02 13:12:36 +0800

    refactor

--- EasyFrameWork.CMS/Filter/EditWidgetAttribute.cs
+++ EasyFrameWork.CMS/Filter/EditWidgetAttribute.cs
@@ -13,66 +13,17 @@ using Easy.Extend;
 
 namespace Easy.Web.CMS.Filter
 {
-    public class EditWidgetAttribute : FilterAttribute, IActionFilter
+    public class EditWidgetAttribute : WidgetAttribute
     {
-
-        public void OnActionExecuted(ActionExecutedContext filterContext)
+        public override PageEntity GetPage(ActionExecutedContext filterContext)
         {
-            ZoneWidgetCollection zones = new ZoneWidgetCollection();
-
-            //Page
             string pageId = filterContext.RequestContext.HttpContext.Request.QueryString["ID"];
-            PageService pageService = new PageService();
-            PageEntity page = pageService.Get(pageId);
-
-            if (page != null)
-            {
-                LayoutService layoutService = new LayoutService();
-                var layout = layoutService.Get(page.LayoutId);
-                layout.Page = page;
-                WidgetService widgetService = new WidgetService();
-                IEnumerable<WidgetBase> widgets = widgetService.Get(m => m.PageID == page.ID);
-                Action<WidgetBase> processWidget = m =>
-                {
-                    IWidgetPartDriver partDriver = Activator.CreateInstance(m.AssemblyName, m.ServiceTypeName).Unwrap() as IWidgetPartDriver;
-                    WidgetPart part = partDriver.Display(partDriver.GetWidget(m), filterContext.HttpContext);
-                    lock (zones)
-                    {
-                        if (zones.ContainsKey(part.Widget.ZoneID))
-                        {
-                            zones[part.Widget.ZoneID].Add(part);
-                        }
-                        else
-                        {
-                            WidgetCollection partCollection = new WidgetCollection();
-                            partCollection.Add(part);
-                            zones.Add(part.Widget.ZoneID, partCollection);
-                        }
-                    }
-                };
-                widgets.Each(processWidget);
-
-                IEnumerable<WidgetBase> Layoutwidgets = widgetService.Get(m => m.LayoutID == page.LayoutId);
-
-                Layoutwidgets.Each(processWidget);
-
-                layout.ZoneWidgets = zones;
-                ViewResult viewResult = (filterContext.Result as ViewResult);
-                if (viewResult != null)
-                {
-                    viewResult.MasterName = "~/Modules/Common/Views/Shared/_DesignPageLayout.cshtml";
-                    viewResult.ViewData[LayoutEntity.LayoutKey] = layout;
-                }
-            }
-            else
-            {
-                filterContext.Result = new HttpStatusCodeResult(404);
-            }
+            return new PageService().Get(pageId);
         }
 
-        public void OnActionExecuting(ActionExecutingContext filterContext)
+        public override string GetLayout()
         {
-
+            return "~/Modules/Common/Views/Shared/_DesignPageLayout.cshtml";
         }
     }
 
--- EasyFrameWork.CMS/Filter/WidgetAttribute.cs
+++ EasyFrameWork.CMS/Filter/WidgetAttribute.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-using Easy.Data;
-using Easy.Web.CMS.Widget;
+﻿using Easy.Web.CMS.Widget;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,66 +16,78 @@ namespace Easy.Web.CMS.Filter
 {
     public class WidgetAttribute : FilterAttribute, IActionFilter
     {
+        public ZoneWidgetCollection Zones { get; set; }
 
-        public void OnActionExecuted(ActionExecutedContext filterContext)
+        public virtual PageEntity GetPage(ActionExecutedContext filterContext)
         {
-            var zones = new ZoneWidgetCollection();
-            var cache = new StaticCache();
-            //Page
             string path = filterContext.RequestContext.HttpContext.Request.Path;
             if (path.EndsWith("/") && path.Length > 1)
             {
                 path = path.Substring(0, path.Length - 1);
-                //filterContext.HttpContext.Response.Redirect(path);
                 filterContext.Result = new RedirectResult(path);
-                return;
+                return null;
             }
-            bool publish = !ReView.Review.Equals(filterContext.RequestContext.HttpContext.Request.QueryString[ReView.QueryKey], StringComparison.CurrentCultureIgnoreCase);
-            var page = new PageService().GetByPath(path, publish);
+            bool publish = true;
+            if (filterContext.RequestContext.HttpContext.Request.IsAuthenticated)
+            {
+                publish = !ReView.Review.Equals(
+                    filterContext.RequestContext.HttpContext.Request.QueryString[ReView.QueryKey],
+                    StringComparison.CurrentCultureIgnoreCase);
+            }
+
+            return new PageService().GetByPath(path, publish);
+        }
+
+        public virtual string GetLayout()
+        {
+            return null;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Zones = new ZoneWidgetCollection();
+            //Page
+            var page = GetPage(filterContext);
             if (page != null)
             {
+                var cache = new StaticCache();
                 var layoutService = new LayoutService();
                 LayoutEntity layout = layoutService.Get(page.LayoutId);
                 layout.Page = page;
-
                 Action<WidgetBase> processWidget = m =>
                 {
                     IWidgetPartDriver partDriver = cache.Get("IWidgetPartDriver_" + m.AssemblyName + m.ServiceTypeName, source =>
                          Activator.CreateInstance(m.AssemblyName, m.ServiceTypeName).Unwrap() as IWidgetPartDriver
                     );
                     WidgetPart part = partDriver.Display(partDriver.GetWidget(m), filterContext.HttpContext);
-                    lock (zones)
+                    lock (Zones)
                     {
-                        if (zones.ContainsKey(part.Widget.ZoneID))
+                        if (Zones.ContainsKey(part.Widget.ZoneID))
                         {
-                            zones[part.Widget.ZoneID].Add(part);
+                            Zones[part.Widget.ZoneID].Add(part);
                         }
                         else
                         {
                             var partCollection = new WidgetCollection { part };
-                            zones.Add(part.Widget.ZoneID, partCollection);
+                            Zones.Add(part.Widget.ZoneID, partCollection);
                         }
                     }
                 };
-                var layoutWidgetsTask = Task.Factory.StartNew(layoutId =>
-                {
-                    var widgetServiceIn = new WidgetService();
-                    IEnumerable<WidgetBase> layoutwidgets = widgetServiceIn.Get(m => m.LayoutID == layoutId.ToString());
-                    layoutwidgets.Each(processWidget);
-                }, page.LayoutId);
-
                 var widgetService = new WidgetService();
-                IEnumerable<WidgetBase> widgets = widgetService.Get(m => m.PageID == page.ID);
-                int middle = widgets.Count() / 2;
-                var pageWidgetsTask = Task.Factory.StartNew(() => widgets.Skip(middle).Each(processWidget));
-                widgets.Take(middle).Each(processWidget);
-                Task.WaitAll(new[] { pageWidgetsTask, layoutWidgetsTask });
+                List<WidgetBase> widgets = widgetService.Get(m => m.LayoutID == page.LayoutId).ToList();
+                widgets.AddRange(widgetService.Get(m => m.PageID == page.ID));
+                var parallel = from widget in widgets.AsParallel() select widget;
+                parallel.ForAll(processWidget);
 
-                layout.ZoneWidgets = zones;
+                layout.ZoneWidgets = Zones;
                 var viewResult = (filterContext.Result as ViewResult);
                 if (viewResult != null)
                 {
-                    //viewResult.MasterName = "~/Modules/Easy.Web.CMS.Page/Views/Shared/_Layout.cshtml";
+                    string layoutView = GetLayout();
+                    if (layoutView.IsNotNullAndWhiteSpace())
+                    {
+                        viewResult.MasterName = layoutView;
+                    }
                     viewResult.ViewData[LayoutEntity.LayoutKey] = layout;
                 }
             }
