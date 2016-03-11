bafeeff2f4d8f697531f5d8c3e7b87af80f79b1c
Wayne <seriawei@gmail.com>
2016-03-11 16:35:48 +0800

    new data

--- DataBase/InitialData/dbo.ProductCategoryWidget.Table.sql
+++ DataBase/InitialData/dbo.ProductCategoryWidget.Table.sql
@@ -1,3 +1,3 @@
-INSERT [dbo].[ProductCategoryWidget] ([ID], [ProductCategoryID], [TargetPage]) VALUES (N'543cb419c9224f0b8eba789b802ea183', 1, NULL)
+INSERT [dbo].[ProductCategoryWidget] ([ID], [ProductCategoryID], [TargetPage]) VALUES (N'cdbf7482b7b74d39b93a6af32c63e13a', 1, NULL)
 INSERT [dbo].[ProductCategoryWidget] ([ID], [ProductCategoryID], [TargetPage]) VALUES (N'eb7657f5bcbd4e98977a42950936e7ac', 1, NULL)
 GO
--- DataBase/InitialData/dbo.ProductListWidget.Table.sql
+++ DataBase/InitialData/dbo.ProductListWidget.Table.sql
@@ -1,3 +1,3 @@
-INSERT [dbo].[ProductListWidget] ([ID], [IsPageable], [ProductCategoryID], [DetailPageUrl], [Columns], [PageSize]) VALUES (N'605e5dedbe8242c4b8d20b88b0e7fec2', 1, 1, N'~/product/detail', N'col-xs-6 col-sm-4 col-md-4', 9)
+INSERT [dbo].[ProductListWidget] ([ID], [IsPageable], [ProductCategoryID], [DetailPageUrl], [Columns], [PageSize]) VALUES (N'e43316dbd8f04f559419bfc4085e21ce', 1, 1, N'~/product/detail', N'col-xs-6 col-sm-4 col-md-4', 9)
 INSERT [dbo].[ProductListWidget] ([ID], [IsPageable], [ProductCategoryID], [DetailPageUrl], [Columns], [PageSize]) VALUES (N'e88916cc41b3465b9ae954bafac5f796', 1, 1, N'~/product/detail', N'col-xs-6 col-sm-4 col-md-4', 9)
 GO
