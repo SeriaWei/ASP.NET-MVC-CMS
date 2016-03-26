fa228d442bbacb3d9f221747bd57a9af049b72ed
Wayne <seriawei@gmail.com>
2016-03-26 21:16:00 +0800

    Update Standard Module

--- Easy.CMS.Web/Modules/Standard/Properties/AssemblyInfo.cs
+++ Easy.CMS.Web/Modules/Standard/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("PlugWeb.Product")]
+[assembly: AssemblyTitle("Easy.CMS.Standard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PlugWeb.Product")]
+[assembly: AssemblyProduct("Easy.CMS.Standard")]
 [assembly: AssemblyCopyright("版权所有(C)  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
