323eba0a800ca285d02ef69b79e9ea62a4eca048
Wayne <seriawei@gmail.com>
2016-07-21 16:47:53 +0800

    .

--- DataBase/Update/CMS_WidgetTemplate.sql
+++ DataBase/Update/CMS_WidgetTemplate.sql
@@ -1,4 +1,18 @@
+CREATE TABLE [dbo].[StyleSheetWidget](
+	[ID] [nvarchar](100) NOT NULL,
+	[StyleSheet] [nvarchar](max) NULL,
+ CONSTRAINT [PK_StyleSheetWidget] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+GO
+ALTER TABLE [dbo].[StyleSheetWidget]  WITH CHECK ADD  CONSTRAINT [FK_StyleSheetWidget_CMS_WidgetBase] FOREIGN KEY([ID])
+REFERENCES [dbo].[CMS_WidgetBase] ([ID])
+GO
+ALTER TABLE [dbo].[StyleSheetWidget] CHECK CONSTRAINT [FK_StyleSheetWidget_CMS_WidgetBase]
+GO
 
 
 INSERT INTO dbo.CMS_WidgetTemplate
