b85401d5e9ffd244a301038a8f7933fd22ac415c
Wayne <seriawei@gmail.com>
2016-08-24 11:56:34 +0800

    .

--- Easy.CMS.Web/Modules/Breadcrumb/DBScripts/Init.sql
+++ Easy.CMS.Web/Modules/Breadcrumb/DBScripts/Init.sql
@@ -1,9 +1,21 @@
-CREATE TABLE BreadcrumbWidget
-    (
-      ID NVARCHAR(100) PRIMARY KEY
-                       NOT NULL ,
-      IsLinkAble BIT NULL
-    );
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BreadcrumbWidget](
+	[ID] [nvarchar](100) NOT NULL,
+	[IsLinkAble] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[BreadcrumbWidget]  WITH CHECK ADD  CONSTRAINT [FK_BreadcrumbWidget_CMS_WidgetBase] FOREIGN KEY([ID])
+REFERENCES [dbo].[CMS_WidgetBase] ([ID])
+GO
+ALTER TABLE [dbo].[BreadcrumbWidget] CHECK CONSTRAINT [FK_BreadcrumbWidget_CMS_WidgetBase]
 GO
 INSERT INTO dbo.CMS_WidgetTemplate
         ( Title ,
