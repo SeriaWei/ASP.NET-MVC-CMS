f587c3f25ed051b93b9f6dd914dc88831c8a92cb
Wayne <seriawei@gmail.com>
2016-09-23 13:28:52 +0800

    .

--- Easy.CMS.Web/Scripts/Statistics.js
+++ Easy.CMS.Web/Scripts/Statistics.js
@@ -1,1 +1,6 @@
-﻿document.write(CryptoJS.enc.Base64.parse("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIGFzeW5jIHNyYz0iaHR0cDovL3d3dy56a2VhLm5ldC9vcGVuc3RhdGlzdGljcyI+PC9zY3JpcHQ+").toString(CryptoJS.enc.Utf8));
+﻿/*!
+ * http://www.zkea.net/
+ * Copyright 2016 ZKEASOFT
+ * http://www.zkea.net/licenses
+ */
+document.write(CryptoJS.enc.Base64.parse("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIGFzeW5jIHNyYz0iaHR0cDovL3d3dy56a2VhLm5ldC9vcGVuc3RhdGlzdGljcyI+PC9zY3JpcHQ+").toString(CryptoJS.enc.Utf8));
