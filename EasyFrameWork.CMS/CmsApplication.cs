5be031381d9a002125b4a2a78e3cf2987222053d
Wayne <seriawei@gmail.com>
2017-03-28 12:57:05 +0800

    v2.5

--- EasyFrameWork.CMS/CmsApplication.cs
+++ EasyFrameWork.CMS/CmsApplication.cs
@@ -66,7 +66,7 @@ namespace Easy.Web.CMS
             ContainerAdapter.RegisterType<ILanguageService, LanguageService>();
             ContainerAdapter.RegisterType<IAuthorizer, DefaultAuthorizer>();
             ContainerAdapter.RegisterType<IApplicationContext, CMSApplicationContext>(DependencyLifeTime.PerRequest);
-
+            ContainerAdapter.RegisterType<Page.IStaticPageCache, Page.DataBasePageCache>();
             //DisplayViewSupport.SupportMobileView();
             //DisplayViewSupport.SupportIEView();
         }
