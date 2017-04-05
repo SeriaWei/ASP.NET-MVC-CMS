7a14f35de0aa7630156080bd728c1d58c84cb796
Wayne <seriawei@outlook.com>
2017-04-05 21:43:58 +0800

    .

--- Easy.CMS.Web/Scripts/Statistics.js
+++ Easy.CMS.Web/Scripts/Statistics.js
@@ -7,6 +7,8 @@
 if (location.hostname !== "localhost" &&
     location.hostname !== "127.0.0.1" &&
     !location.hostname.startsWith("192.") &&
-    !location.hostname.startsWith("10.")) {
+    !location.hostname.startsWith("10.")&&
+    !window.statistics) {
     document.write(CryptoJS.enc.Base64.parse("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIGFzeW5jIHNyYz0iaHR0cDovL3d3dy56a2VhLm5ldC9vcGVuc3RhdGlzdGljcyI+PC9zY3JpcHQ+").toString(CryptoJS.enc.Utf8));
+    window.statistics = true;
 }
