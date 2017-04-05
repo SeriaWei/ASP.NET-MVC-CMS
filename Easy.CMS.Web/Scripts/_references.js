46d283945ee45866cef3d012644a5ee59e8ff84a
Wayne <seriawei@outlook.com>
2017-04-05 22:24:23 +0800

    .

--- Easy.CMS.Web/Scripts/_references.js
+++ Easy.CMS.Web/Scripts/_references.js
@@ -1,4 +1,11 @@
-﻿/// <reference path="jquery-ui-1.11.4.js" />
+﻿/*!
+ * http://www.zkea.net/
+ * Copyright 2017 ZKEASOFT
+ * Before you remove this script, please review the lincese below
+ * http://www.zkea.net/licenses
+ */
+
+/// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="jquery-2.2.3.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
