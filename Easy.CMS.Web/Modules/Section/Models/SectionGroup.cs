87233d267f3e2463b150d10404ceb0afd6cd8dee
Wayne <seriawei@gmail.com>
2017-05-11 14:44:08 +0800

    Order

--- Easy.CMS.Web/Modules/Section/Models/SectionGroup.cs
+++ Easy.CMS.Web/Modules/Section/Models/SectionGroup.cs
@@ -35,7 +35,7 @@ namespace Easy.CMS.Section.Models
         {
             if (SectionContents != null)
             {
-                return SectionContents.Where(m => m != null && m.SectionContentType == (int)type).Cast<T>();
+                return SectionContents.Where(m => m != null && m.SectionContentType == (int)type).OrderBy(m => m.Order).Cast<T>();
             }
             return null;
         }
@@ -102,7 +102,7 @@ namespace Easy.CMS.Section.Models
         {
             if (_templateName.IsNullOrWhiteSpace())
             {
-                var template= ServiceLocator.Current.GetInstance<ISectionTemplateService>().Get(PartialView);
+                var template = ServiceLocator.Current.GetInstance<ISectionTemplateService>().Get(PartialView);
                 if (template != null)
                 {
                     _templateName = template.Title;
